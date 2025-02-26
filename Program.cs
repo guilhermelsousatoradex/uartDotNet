dbef65efdd1fad76520e843335978dcf9b72418c
Guilherme L Sousa <guilherme.sousa@toradex.com>
2025-02-26 14:42:40 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading;
+using NmeaParser;
+using NmeaParser.Messages;
+using System.IO.Ports;
+
+namespace gps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort serialDev;
+            SerialPortDevice gps;
+
+            // serial configs
+            serialDev = new SerialPort();
+         //   serialDev.PortName = Environment.GetEnvironmentVariable("GPS_SERIAL_PORT");
+            serialDev.PortName = "/dev/colibri-uartc";
+            serialDev.BaudRate = 9600;
+            Console.WriteLine($"Latitude::\tLongitude::");
+            gps = new SerialPortDevice(serialDev);
+
+            // set listener
+            gps.MessageReceived += OnNmeaMessageReceived;
+            gps.OpenAsync();
+
+            // on ctrl+c close connection
+            Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs eventArgs) =>
+            {
+                gps.CloseAsync();
+            };
+
+            // wait for gps inputs
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        static void OnNmeaMessageReceived(object sender, NmeaMessageReceivedEventArgs args)
+        {
+            if (args.Message is Rmc rmc)
+            {
+                Console.WriteLine($"Latitude::{rmc.Latitude}\tLongitude::{rmc.Longitude}");
+            }
+        }
+    }
+}
